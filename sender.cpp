4b5bc5899fa4888a02557f7e1c98d766931f9246
jonesjardel <jonesjardel@gmail.com>
2015-05-02 13:27:41 -0300

    Update sender.cpp

--- sender.cpp
+++ sender.cpp
@@ -19,44 +19,20 @@
 #include <Ws2tcpip.h>
 #include <iostream>
 #include <cstdlib>
+#include <ipsla.h>
 using namespace std;
 
-//Define variáveis do protocolo de controle
+/*Define variáveis do protocolo de controle*/
 
 #define SENDER_CONTROL_PORT	3000
 #define RESPONDER_CONTROL_PORT 1167
 
-/*Define as variáveis utilizadas durante a sessão de controle do IPSLA para o envio de mensagens de controle*/
-struct control_request_message
-{
-	__int32 sequence_number,total_length;
-	__int8 version,reserved;
-	char send_timestamp;
-
-	struct CSLD_authentication
-	{
-		__int16 command,status,keyid;
-		__int32 command_length;
-		__int8 mode,reserved;
-		long random_number;
-		long message_authentication_digest;
-	};
-
-	struct CSLD_measurement
-	{
-		__int8 address_type,role;
-		__int16 command,status,reserved,reserved2,control_source_port,measurement_source_port,measurement_destination_port;
-		__int32 command_length,session_identifier,duration;
-		long control_source_address,control_destination_address,measurement_source_address,measurement_destination_address;
-	};
-};
-
-
 int main (void)
 {
-	control_request_message command_header;
-	control_request_message::CSLD_authentication authentication;
-	control_request_message::CSLD_measurement measurement;
+	control_phase command_header;
+	measurement_phase mp;
+	control_phase::CSLD_authentication authentication;
+	control_phase::CSLD_measurement measurement;
 	authentication.status=0;
 	authentication.mode=0;
 	authentication.reserved=0;
@@ -67,7 +43,6 @@ int main (void)
 	measurement.reserved2=0;
 	command_header.version=2;
 	command_header.reserved=0;
-
 
 	printf("Escreva ja!\n");
     return (0);
