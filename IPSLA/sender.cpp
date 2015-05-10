ab3922d4255b93ff1cc3bf69dabe4ab08b8948bd
jonesjardel <jonesjardel@gmail.com>
2015-05-10 19:30:11 -0300

    Update sender.cpp

--- IPSLA/sender.cpp
+++ IPSLA/sender.cpp
@@ -35,13 +35,27 @@ struct client_info
 
 static int control_request_msg (struct client_info *client)
 {
-	int socket = client->socket;
 	control_phase::command_header cmd_header;
 	cmd_header.version=2;
 	cmd_header.reserved=0;
 	cmd_header.status=0;
 	cmd_header.sequence_number=0;
 	cmd_header.sequence_number++;
+
+	int socket = client->socket;
+	memset(&cmd_header,0,sizeof(cmd_header));
+	
+	int return_ctrl_req_msg=send(socket,reinterpret_cast<const char*>(&cmd_header),sizeof(cmd_header),0);
+	if (return_ctrl_req_msg < 0)
+	{
+		printf ("[%s]",inet_ntoa(client->addr.sin_addr));
+		perror ("Failed to send control request message");
+	}
+	else
+	{
+		printf ("Sent control request message to %s. Result %d\n",inet_ntoa(client->addr.sin_addr),return_ctrl_req_msg);
+	}
+	return return_ctrl_req_msg;
 }
 
 int main (void)
@@ -57,10 +71,8 @@ int main (void)
 	measurement.role=1;
 	measurement.reserved=0;
 	measurement.reserved2=0;
-	control_request_msg(client	
-	int size_obj=sizeof(struct control_phase);
-	printf("Tamanho da struct control_phase eh: %d\n",size_obj);
-	char tecla;
-	tecla=getchar();
-    return (0);
+	
+
+
+    return (EXIT_SUCCESS);
 }
